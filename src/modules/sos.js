e4d2f44cbafbc91891ab497a9a25a950d6e0d70c
Necrotex <necrotex@gmail.com>
2016-12-13 21:18:34 +0100

    added SwordOrSheath module

---
+++ src/modules/sos.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const Module = require('../module');
+const request = require('request');
+
+
+class Haiku extends Module {
+    trigger(){
+        return "sos";
+    }
+
+    handle(message){
+        request('https://imgur.com/r/SwordOrSheath/hot.json', (error, response, body) => {
+
+            if(!error && response.statusCode == 200) {
+                var msg;
+                var data = JSON.parse(body)['data'];
+
+                var redhead = data[Math.floor(Math.random()*data.length)];
+
+                msg = `http://imgur.com/${redhead.hash}${redhead.ext.replace(/\?.*/, '')}`;
+
+            } else {
+                msg = "Something went wrong. Best ping @Grimm#0928";
+            }
+
+            message.channel.sendMessage(msg);
+        });
+    }
+}
+
+module.exports = function(bot) {
+    new Haiku(bot);
+};
