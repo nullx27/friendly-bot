6233f390ce877959a2bba13342a1f7ba1318bdcd
Crowdedlight <crowdedhd@gmail.com>
2016-12-24 12:50:03 +0100

    latest wip, discord char limit per msg is rejecting some posts

--- src/modules/tfts.js
+++ src/modules/tfts.js
@@ -25,15 +25,23 @@ class tfts extends Module {
             if(!error && response.statusCode == 200) {
                 var data = JSON.parse(body);
                 var post = data[0].data.children[0].data;
-                var text = post.selftext.replace(/&gt;/g, ">").replace(/&lt;/g, "<");
+                var text = post.selftext.replace(/&gt;/g, ">").replace(/&lt;/g, "<"); //.replace(/\\([\s\S])|(")/g,"\\$1$2");
+                //console.log(text);
 
-                msg = "```"
-                        + "**" + post.title + "**"
-                        + "\n\n"
-                        + text + "```";
+                //msg = "```"
+                //        + "**" + post.title + "**"
+                //        + "\n\n"
+                //        + text + "\n" + "```";
+
+                msg = `**${post.title}** \n\n${text} \n`;
+                msg = "```" + msg + "```";
+
+
             } else {
                 msg = "Something went wrong. Best ping @Crow LightBringer#7621";
             }
+            console.log("sent msg");
+            console.log(msg);
             sentMsg.then(message => {message.edit(msg)});
         });
     }
