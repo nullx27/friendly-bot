a2799ffc3a81eab1c904e7e0eb8c2f1174869e3a
Crowdedlight <crowdedhd@gmail.com>
2016-12-24 02:32:14 +0100

    moved apikey to config

---
+++ src/modules/youtube.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const Module = require('../module');
+const request = require('request');
+
+var maxRandom = 0;
+class tfts extends Module {
+
+    trigger(){
+        return "youtube";
+    }
+
+    help(){
+        return "Gets first result from youtube." + "\n\n" + "!youtube < search >";
+    }
+
+    handle(message){
+        var msg;
+        var search;
+        var send = true;
+
+        //try block to catch exception if no command is given
+        try {
+            search = message.content.substring(1).trim().slice(7).trim();
+        }
+        catch (e) {
+            search = "";
+        }
+
+        if (search == "")
+        {
+            send = false;
+            message.channel.sendMessage("You need to enter something to search for, you dumbass!");
+        }
+
+
+        var key = this.bot.config.youtubeAPI;
+
+        var host = `https://content.googleapis.com/youtube/v3/search?part=snippet&q=${search}&safeSearch=moderate&key=${key}`;
+
+        if (send)
+        {
+            request(host, (error, response, body) => {
+                if(!error && response.statusCode == 200) {
+                    var data = JSON.parse(body);
+                    var videolink = data.items[0].id.videoId;
+
+                    msg = `https://www.youtube.com/watch?v=${videolink}`;
+                } else {
+                    msg = "Something went wrong. Best ping @Crow LightBringer#7621";
+                }
+                message.channel.sendMessage(msg);
+            });
+        }
+    }
+}
+
+module.exports = function(bot) {
+    new tfts(bot);
+};
