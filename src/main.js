5c2c93e33a2bad68b2063d87110a28054b2816e8
Andre Peiffer <necrotex@gmail.com>
2017-06-04 21:37:34 +0200

    Added remindme command

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 'use strict';
 
-var autoload = require('auto-load');
-var config = require('../config.json');
+let autoload = require('auto-load');
+let config = require('../config.json');
 
-var Bot = require('./friendly-bot');
-var modules = autoload(__dirname + '/modules');
+let Bot = require('./friendly-bot');
+let modules = autoload(__dirname + '/modules');
 
-var bot = new Bot(config);
+let bot = new Bot(config);
 
-for(var module in modules){
+for(let module in modules){
      modules[module](bot);
 }
 
