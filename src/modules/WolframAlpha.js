2d95f687841a0da693c593b4c5687edc64af7f6c
Crowdedlight <crowdedhd@gmail.com>
2017-10-24 14:17:47 +0200

    updated wolframalpha to show plots and use embed method

--- src/modules/WolframAlpha.js
+++ src/modules/WolframAlpha.js
@@ -2,6 +2,7 @@
 
 const Module = require('../module');
 const request = require('request');
+const Discord = require('discord.js');
 
 class WolframAlpha extends Module {
 
@@ -31,19 +32,20 @@ class WolframAlpha extends Module {
             input = "";
         }
 
-        if (input == "")
+        if (input != "")
         {
-            send = false;
-            msg = "You need to enter something to calculate, you dumbass!";
-        }
-
-        if (send)
-        {
-            host = `https://api.wolframalpha.com/v2/query?input=${input}&format=plaintext&output=JSON&appid=${api}`;
+            host = `https://api.wolframalpha.com/v2/query?input=${input}&output=JSON&appid=${api}`; //format=plaintext&
 
             request(host, (error, response, body) => {
                 if (!error && response.statusCode == 200) {
                     var data = JSON.parse(body);
+
+                    //start embed
+                    const embed = new Discord.RichEmbed()
+                        .setAuthor("Wolfram Alpha", "https://i.imgur.com/YVWvjlM.png")
+                        .setColor(0x00AE86)
+                        .setFooter("Requested by " + message.author.username, message.author.avatarURL)
+                        .setTimestamp();
 
                     //check for success
                     if (!data.queryresult.success) {
@@ -52,24 +54,33 @@ class WolframAlpha extends Module {
                         //Correct input, can parse output
                         var subAnswers = data.queryresult.pods;
                         //format message
+
                         //msg = "```";
                         for (let pod of subAnswers) {
                             if (pod.id == "Input" || pod.id == "Result") {
-                                msg += "**" + pod.title + "**" + "\n";
-                                msg += "```" + pod.subpods[0].plaintext + "```" + "\n";
+                                embed.addField(pod.title, "```" + pod.subpods[0].plaintext + "```");
+                            }
+
+                            //plot
+                            if (pod.id == "Plot") {
+                                //embed.addField(pod.title, "\t");
+                                embed.setImage(pod.subpods[0].img.src);
                             }
                         }
                         //replace ? with =
-                        msg = msg.replace(/\uF7D9/g, "=");
+                        //msg = msg.replace(/\uF7D9/g, "=");
                     }
-
+                    //send embed
+                    message.channel.send(embed);
                 } else {
+                    //error send error msg
                     msg = "Something went wrong. Best ping @Crow LightBringer#7621";
+                    message.channel.send(msg);
                 }
-                message.channel.sendMessage(msg);
             });
         } else {
-            message.channel.sendMessage(msg);
+            msg = "You need to enter something to calculate, you dumbass!";
+            message.channel.send(msg);
         }
     }
 }
