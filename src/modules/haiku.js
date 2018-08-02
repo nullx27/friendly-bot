e30a591dc95ec8491bff0219b5116c70d17749f5
Andre Peiffer <necrotex@gmail.com>
2018-08-02 13:21:02 +0200

    updated haiku command

--- src/modules/haiku.js
+++ src/modules/haiku.js
@@ -4,27 +4,38 @@ const Module = require('../module');
 const request = require('request');
 
 class Haiku extends Module {
-    trigger(){
+    trigger() {
         return "haiku";
     }
 
     help() {
         return "Get a inspireing haiku about a DAMN PATRIOT's sexual prowess" +
-        "\n\n" +
-        "Available commands:\n" +
-        "!haiku";
+            "\n\n" +
+            "Available commands:\n" +
+            "!haiku";
     }
 
-    handle(message){
-        request('http://baers.nimos.ws/api/haiku', (error, response, body) => {
+    handle(message) {
+
+        let url = "https://auth.friendlyprobes.net/api/haiku?api_token=" + this.bot.config.asgard_api_token
+
+        try {
+            let input = message.content.substring(1).trim().slice(5).trim();
+            url = `https://auth.friendlyprobes.net/api/haiku/${input}?api_token=` + this.bot.config.asgard_api_token
+        }
+        catch (e) {
+        }
+
+        request(url, (error, response, body) => {
             var msg;
-            if(!error && response.statusCode == 200) {
+            if (!error && response.statusCode == 200) {
                 var haiku = JSON.parse(body);
+                haiku = haiku.data;
 
                 msg = '```';
                 msg += haiku.text;
                 msg += '\r\r';
-                msg += '-' + haiku.authorName;
+                msg += '-' + haiku.author;
                 msg += '```';
 
             } else {
@@ -36,6 +47,6 @@ class Haiku extends Module {
     }
 }
 
-module.exports = function(bot) {
+module.exports = function (bot) {
     new Haiku(bot);
 };
