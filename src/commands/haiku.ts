4c96d9f6532c8aafc23e8f8bde4eb90d399c4507
Andre Peiffer <necrotex@gmail.com>
2019-08-13 16:32:42 +0200

    Updated the haiku command to be searchable

--- src/commands/haiku.ts
+++ src/commands/haiku.ts
@@ -5,22 +5,44 @@ import fs from "fs";
 import {getRandomInt} from "../core/utils/Helpers";
 import {Reply} from "../core/models/messages/Reply";
 import {Container} from "../core/utils/Container";
+import {Help} from "../core/models/messages/Help";
 
 @trigger('haiku')
 class Haiku extends Command {
-    protected lines: Haiku[] = [];
+    protected haikus: Haiku[] = [];
 
     constructor(container: Container) {
         super(container);
         const file = fs.readFileSync(__dirname + '/../../data/haiku.json', 'utf8');
-        this.lines = JSON.parse(file);
+        this.haikus = JSON.parse(file);
+    }
+
+    public help(): Help {
+        return new Help()
+            .addTitle('Haiku')
+            .addDescription('Get an inspiring Haiku about the sexual prowess of the eternal god king of Dropbaers Anonymous')
+            .addCommand('haiku', 'Get a random haiku')
+            .addCommand('haiku <author>', 'Search for a random haiku of a specific author');
     }
 
     async handle(message: Discord.Message, args: string[]) {
-        let line = this.lines[getRandomInt(0, this.lines.length - 1)];
+        let haikus: any = [];
+
+        if (args[0]) {
+            for (let item of this.haikus) {
+                if (item.author.toLowerCase().includes(args[0].toLowerCase())) {
+                    haikus.push(item);
+                }
+            }
+        }
+        let haiku: any;
+
+        if (haikus.length > 0) haiku = haikus[getRandomInt(0, haikus.length)];
+        else haiku = this.haikus[getRandomInt(0, this.haikus.length)];
+
         new Reply(message)
             .setTitle('Haiku')
-            .addField(line.author, line.text.replace(/^"+|"+$/g, ''), true).send();
+            .addField(haiku.author, haiku.text.replace(/^"+|"+$/g, ''), true).send();
     }
 }
 
